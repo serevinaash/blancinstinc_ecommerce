4a79a31792b72e805476f194c2417082ad1293ed
nfzmhn <zidninurfauzi2203@gmail.com>
2024-10-28 15:23:02 +0700

    Blade Header progres

--- resources/views/components/frontend/header.blade.php
+++ resources/views/components/frontend/header.blade.php
@@ -1,44 +1,26 @@
-<!DOCTYPE html>
-<html class="no-js" lang="">
-  <head>
-    <meta charset="utf-8" />
-    <title>BlancInstinc</title>
-    <meta name="description" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>BlancInstinc - Health Supplement Landing Page</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta property="og:title" content="" />
-    <meta property="og:type" content="" />
-    <meta property="og:url" content="" />
-    <meta property="og:image" content="" />
- 
-    <link rel="manifest" href="site.webmanifest" />
-    <link rel="apple-touch-icon" href={{url("/frontend/images/content/logo.png")}} />
-    <!-- Place favicon.ico in the root directory -->
+		<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+        <!-- Place favicon.ico in the root directory -->
 
-    {{-- <link rel="stylesheet" href="{{url("/frontend/css/main.css")}}" /> --}}
-    <link rel="icon" href={{url("/frontend/images/content/logo.png")}} />
-
-    <meta name="theme-color" content="#000" />
-    <link rel="icon" href="{{url("/frontend/images/content/logo.png")}}">
-    <link href="{{url("/frontend/css/app.minify.css")}}" rel="stylesheet">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <script>
-  document.addEventListener('DOMContentLoaded', function () {
-    var brandDropdown = document.getElementById('brand-dropdown');
-    
-    brandDropdown.addEventListener('mouseenter', function () {
-      var brandDropdownMenu = document.querySelector('#brand-dropdown > ul');
-      brandDropdownMenu.classList.remove('hidden');
-    });
-
-    brandDropdown.addEventListener('mouseleave', function () {
-      var brandDropdownMenu = document.querySelector('#brand-dropdown > ul');
-      brandDropdownMenu.classList.add('hidden');
-    });
-  });
-</script>
-
-   </head>
-   
-   <body>
+		<!-- CSS here -->
+        <link rel="stylesheet" href={{url("/mppl/assets/css/bootstrap.min.css")}}>
+        <link rel="stylesheet" href={{url("/mppl/assets/css/animate.min.css")}}>
+        <link rel="stylesheet" href={{url("/mppl/assets/css/magnific-popup.css")}}>
+        <link rel="stylesheet" href={{url("/mppl/assets/css/fontawesome-all.min.css")}}>
+        <link rel="stylesheet" href={{url("/mppl/assets/css/flaticon.css")}}>
+        <link rel="stylesheet" href={{url("/mppl/assets/css/jquery-ui.css")}}>
+        <link rel="stylesheet" href={{url("/mppl/assets/css/odometer.css")}}>
+        <link rel="stylesheet" href={{url("/mppl/assets/css/slick.css")}}>
+        <link rel="stylesheet" href={{url("/mppl/assets/css/default.css")}}>
+        <link rel="stylesheet" href={{url("/mppl/assets/css/style.css")}}>
+        <link rel="stylesheet" href={{url("/mppl/assets/css/responsive.css")}}>
+    </head>
+  </html>

0fd2c8473999f3f794ad2dd1feb8021c2eb4893e
serevina sherly maulida <101394654+serevinaash@users.noreply.github.com>
2024-11-20 11:11:10 +0700

    dashboard

--- resources/views/pages/frontend/index.blade.php
+++ resources/views/pages/frontend/index.blade.php
@@ -21,7 +21,7 @@
                         <p class=" wow fadeInUp" data-wow-delay=".6s">Pada akhirnya ini bukan hanya soal aroma, tapi juga soal mengingatkan akan sumber kekuatan dalam diri sendiri mengajak untuk
                         </p>
                         <div class="tg-banner-btn wow fadeInUp" data-wow-delay=".8s">
-                            <a href="shop-details.html" class="tg-btn">purchase now</a>
+                            <a href="{{route ("shopDetails")}}" class="tg-btn">purchase now</a>
                         </div>
                     </div>
                 </div>
@@ -35,38 +35,41 @@
         <div class="container">
             <div class="tg-features-inner">
                 <div class="row justify-content-center">
+                    <!-- Parfum Wanita -->
                     <div class="col-lg-4 col-md-6 col-sm-8 wow fadeInUp" data-wow-delay=".2s">
                         <div class="tg-features-item">
                             <div class="tg-features-icon">
                                 <i class="flaticon-supplement"></i>
                             </div>
                             <div class="tg-features-content">
-                                <h4 class="title">Calorie Build Up</h4>
-                                <p>A dietary supplement manfactured supplement thats diets.</p>
+                                <h4 class="title">Parfum Wanita</h4>
+                                <p>Parfum dengan aroma lembut dan elegan, dirancang khusus untuk wanita modern.</p>
                                 <a href="contact.html">Learn more</a>
                             </div>
                         </div>
                     </div>
+                    <!-- Parfum Pria -->
                     <div class="col-lg-4 col-md-6 col-sm-8 wow fadeInUp" data-wow-delay=".4s">
                         <div class="tg-features-item">
                             <div class="tg-features-icon">
                                 <i class="flaticon-whey-protein-3"></i>
                             </div>
                             <div class="tg-features-content">
-                                <h4 class="title">Regular Routine</h4>
-                                <p>A dietary supplement manfactured supplement thats diets.</p>
+                                <h4 class="title">Parfum Pria</h4>
+                                <p>Parfum maskulin dengan aroma segar dan berkarakter, cocok untuk pria aktif.</p>
                                 <a href="contact.html">Learn more</a>
                             </div>
                         </div>
                     </div>
+                    <!-- Parfum Unisex -->
                     <div class="col-lg-4 col-md-6 col-sm-8 wow fadeInUp" data-wow-delay=".6s">
                         <div class="tg-features-item">
                             <div class="tg-features-icon">
                                 <i class="flaticon-strong-1"></i>
                             </div>
                             <div class="tg-features-content">
-                                <h4 class="title">Energy Grow Up</h4>
-                                <p>A dietary supplement manfactured supplement thats diets.</p>
+                                <h4 class="title">Parfum Unisex</h4>
+                                <p>Parfum dengan aroma netral yang dapat digunakan oleh pria maupun wanita.</p>
                                 <a href="contact.html">Learn more</a>
                             </div>
                         </div>
@@ -75,6 +78,7 @@
             </div>
         </div>
     </section>
+
     <!-- tg-features-area-end -->
 
     <!-- supplement-area -->
@@ -85,10 +89,11 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-6 col-md-8 order-0 order-lg-2">
                         <div class="tg-supplement-img text-end  wow fadeInRight" data-wow-delay=".2s">
-                            <img src="assets/img/others/supplement_img.png" alt="">
+                            <img src=" {{ url('/mppl/assets/img/others/supplement_img.png') }}" alt="">
+
                         </div>
                         <div class="tg-supplement-shape">
-                            <img src="assets/img/others/supplement_shape02.png" alt="" class="rotateme">
+                            <img src=" {{ url('/mppl/assets/img/others/supplement_shape02.png') }}" alt="" class="rotateme">
                         </div>
                     </div>
                     <div class="col-lg-6">
@@ -111,25 +116,29 @@
     <section class="tg-video-area tg-video-bg jarallax parallax-img">
         <div class="container">
             <div class="row align-items-center justify-content-center">
+                <!-- Gambar Shopee -->
                 <div class="col-xl-7 col-lg-8">
                     <div class="tg-video-img wow fadeInLeft" data-wow-delay=".2s">
-                        <img src="assets/img/Parfume-Asset/Shoppe.PNG" alt="">
+                        <img src="{{url("/mppl/assets/img/Parfume-Asset/Shoppe.PNG")}}" alt="Visit us on Shopee">
                     </div>
+
                 </div>
+                <!-- Konten Promosi -->
                 <div class="col-xl-5 col-lg-8">
                     <div class="tg-video-content wow fadeInRight" data-wow-delay=".2s">
                         <div class="tg-section-title white mb-30">
-                            <span class="sub-title">promotional</span>
-                            <h2 class="title white-text">Visit us in Shopee!</h2>
+                            <span class="sub-title">Promosi Eksklusif</span>
+                            <h2 class="title white-text">Kunjungi Toko Kami di Shopee!</h2>
                         </div>
-                        <p class="info-one">Vitamin Ipsum is simply dummy text of the printing and tysetting industry. Lorem ipsum has been the industry's standard dummy text ever since the when an unknown.</p>
-                        <p class="info-two">printing and tysetting industry. Lorem ipsum has been thindustry's standard dummy text ever since the when an unknown.</p>
-                        <a href="shop.html" class="tg-btn">VIEW more</a>
+                        <p class="info-one">Dapatkan berbagai pilihan parfum berkualitas dengan penawaran terbaik hanya di Shopee. Tersedia untuk pria, wanita, dan unisex.</p>
+                        <p class="info-two">Belanja mudah, aman, dan hemat dengan promo eksklusif setiap hari. Temukan parfum favorit Anda sekarang juga!</p>
+                        <a href="https://shopee.co.id/blancinstinc?entryPoint=ShopBySearch&searchKeyword=blancinstinc" class="tg-btn">Lihat Selengkapnya</a>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+
     <!-- video-area-end -->
 
     <!-- tg-shop-area -->
@@ -176,126 +185,6 @@
     <!-- tg-shop-area-end -->
 
 
-
-    <!-- shop-details-area -->
-    <section id="shop" class="tg-shop-details-area tg-shop-details-bg">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-6 col-lg-8">
-                    <div class="tg-section-title text-center mb-70">
-                        <span class="sub-title">purchase product</span>
-                        <h2 class="title">take your supplements</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio consequuntur recusandae voluptates sequi voluptatibus error! gorem ipsum dolor sit.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="inner-shop-details-flex-wrap">
-                        <div class="inner-shop-details-img-wrap">
-                            <div class="tab-content" id="myTabContent">
-                                <div class="tab-pane show active" id="item-one" role="tabpanel" aria-labelledby="item-one-tab">
-                                    <div class="inner-shop-details-img">
-                                        <img src={{url("/mppl/assets/img/Parfume-Asset/background2.jpg")}}/>
-                                    </div>
-                                </div>
-                                <div class="tab-pane" id="item-two" role="tabpanel" aria-labelledby="item-two-tab">
-                                    <div class="inner-shop-details-img">
-                                        <img src="assets/img/Parfume-Asset/image_GMn.png" alt="img">
-
-                                    </div>
-                                </div>
-                                <div class="tab-pane" id="item-three" role="tabpanel" aria-labelledby="item-three-tab">
-                                    <div class="inner-shop-details-img">
-                                        <img src="assets/img/Parfume-Asset/image_BGa.png" alt="img">
-                                    </div>
-                                </div>
-                                <div class="tab-pane" id="item-four" role="tabpanel" aria-labelledby="item-four-tab">
-                                    <div class="inner-shop-details-img">
-                                        <img src="assets/img/Parfume-Asset/image_omR.png" alt="img">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="inner-shop-details-nav-wrap">
-                            <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                <li class="nav-item" role="presentation">
-                                    <a href="#" class="nav-link active" id="item-one-tab" data-bs-toggle="tab" data-bs-target="#item-one" role="tab" aria-controls="item-one" aria-selected="true"> <img src={{url("/mppl/assets/img/Parfume-Asset/background2.jpg")}}/></a>
-                                </li>
-                                <li class="nav-item" role="presentation">
-                                    <a href="#" class="nav-link" id="item-two-tab" data-bs-toggle="tab" data-bs-target="#item-two" role="tab"
-                                    aria-controls="item-two" aria-selected="false"><img src="assets/img/Parfume-Asset/image_GMn.png" alt="">
-                                </a>
-                                </li>
-                                <li class="nav-item" role="presentation">
-                                    <a href="#" class="nav-link" id="item-three-tab" data-bs-toggle="tab" data-bs-target="#item-three" role="tab"
-                                    aria-controls="item-three" aria-selected="false"><img src="assets/img/Parfume-Asset/image_BGa.png" alt="">
-
-                                </li>
-                                <li class="nav-item" role="presentation">
-                                    <a href="#" class="nav-link" id="item-four-tab" data-bs-toggle="tab" data-bs-target="#item-four" role="tab" aria-controls="item-four" aria-selected="false"><img src="assets/img/Parfume-Asset/image_omR.png" alt=""></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="inner-shop-details-content">
-                        <h4 class="title">Whey Mass Protein <br> Powder</h4>
-                        <div class="tg-shop-details-review">
-                            <div class="rating">
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                            </div>
-                            <span>( 2 Customer Review )</span>
-                        </div>
-                        <div class="inner-shop-details-price">
-                            <h2 class="price">$29.99</h2>
-                            <h5 class="stock-status">- IN Stock</h5>
-                        </div>
-                        <p>BlancInstinc food is food produced by methods complying with the standards of Rrganic farming. Standards vary Lorem ipsum dolor sit amet consectetur adipiscing worldwide, complying but organic farming.</p>
-                        <div class="tg-progress-wrap">
-                            <div class="tg-progress-content">
-                                <ul class="list-wrap">
-                                    <li>Ordered: <span>20</span></li>
-                                    <li>Items available: <span>360</span></li>
-                                </ul>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 5%;" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                        <div class="inner-shop-perched-info">
-                            <div class="sd-cart-wrap">
-                                <form action="#">
-                                    <div class="quickview-cart-plus-minus">
-                                        <input type="text" value="1">
-                                    </div>
-                                </form>
-                            </div>
-                            <a href="cart.html" class="cart-btn">add to cart</a>
-                            <a href="shop-details.html" class="wishlist-btn" data-bs-toggle="tooltip" data-bs-placement="top" title="Wishlist"><i class="far fa-heart"></i></a>
-                        </div>
-                        <div class="tg-shop-details-list">
-                            <ul class="list-wrap">
-                                <li>SKU : <a href="#">WE888T12</a></li>
-                                <li>Category : <a href="#">growth promoter</a></li>
-                                <li>TAGS : <a href="#">calcium</a></li>
-                            </ul>
-                        </div>
-                        <div class="inner-shop-details-bottom tg-shop-content-bottom">
-                            <p>Free worldwide shipping on all orders over <span>$50</span></p>
-                            <span><i class="far fa-clock"></i>30 days easy returns</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- shop-details-area-end -->
 
     <!-- pricing-area -->
     <section class="tg-pricing-area">

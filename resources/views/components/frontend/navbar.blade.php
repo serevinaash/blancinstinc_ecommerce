9e57424c66e7149264b600f2b00e0089adc1e0a1
serevina sherly maulida <101394654+serevinaash@users.noreply.github.com>
2024-12-02 13:36:18 +0700

    update login logout

--- resources/views/components/frontend/navbar.blade.php
+++ resources/views/components/frontend/navbar.blade.php
@@ -14,20 +14,37 @@
                                     </div>
                                     <div class="navbar-wrap main-menu d-none d-xl-flex">
                                         <ul class="navigation">
-                                          <li class="active menu-item-has-children"><a href={{route ("index") }} class="section-link">Home</a>
+                                          <li class="active menu-item-has-children"><a href={{route ("index") }} class="section-link {{ \Route::current()->getName() === 'index' ? 'md:text-black' : 'md:text-black' }} ">Home</a>
                                             </li>
-                                            <li><a href={{route ("blog") }} class="section-link">Blog</a></li>
+                                            <li><a href={{route ("blog") }} class="section-link  {{ \Route::current()->getName() === 'blog' ? 'md:text-black' : 'md:text-black' }} ">Blog</a></li>
                                             <li class="menu-item-has-children"><a href={{route ("shop")}} class="section-link">Shop</a>
                                       <ul class="sub-menu">
                                                     <li><a href={{route ("shop")}} >Our Shop</a></li>
-                                                    <li><a href={{route ("shopDetails")}}>Shop Details</a></li>
                                                     <li><a href={{route ("cart")}}>Cart Page</a></li>
                                                     <li><a href={{route ("cart")}}>Checkout Page</a></li>
                                                 </ul>
                                             </li>
                                             <li><a href={{route ("contact")}}>Contacts</a></li>
-                                            <li><a href={{route ("login")}} class="section-link">Login</a></li>
-                                            <li><a href={{route ("register")}} class="section-link">Register</a></li>
+
+                                                                            <!-- Logout link -->
+                                            <li><a href="{{ route('logout') }}" class="section-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                                Logout
+                                            </a></li>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                                                @csrf
+                                            </form>
+                                            @auth
+                                            @if(Auth::user()->roles == "ADMIN")
+                                            <li class="mx-3 py-6 md:py-0">
+                                              <a href="{{route("dashboard.index")}}" class="section-link {{\Route::current()->getName() === "index" ? "md:text-white" : "md:text-black"}} hover:underline">Dashboard</a>
+                                            </li>
+                                            @endif
+                                          @endauth
+                                            @guest
+                                            <li><a href={{route ("login")}} class="section-link {{\Route::current()->getName() === "index" ? "md:text-black" : "md:text-black"}}">Login</a></li>
+                                            <li><a href={{route ("register")}} class="section-link  {{\Route::current()->getName() === "index" ? "md:text-black" : "md:text-black"}}">Register</a></li>
+                                            @endguest
+
                                         </ul>
                                     </div>
                                     <div class="header-btn d-none d-sm-block">
